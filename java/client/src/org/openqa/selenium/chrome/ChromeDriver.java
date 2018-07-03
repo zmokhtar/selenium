ccee4a88d0aa899e8ff29e6c6f66fc2438047bd0
Zeki Mokhtarzada <zekitm@gmail.com>
2018-07-03 13:16:15 -0700

    add support for full page chrome screenshots.

--- java/client/src/org/openqa/selenium/chrome/ChromeDriver.java
+++ java/client/src/org/openqa/selenium/chrome/ChromeDriver.java
@@ -19,7 +19,9 @@
 package org.openqa.selenium.chrome;
 
 import com.google.common.collect.ImmutableMap;
+
 import org.openqa.selenium.Capabilities;
+import org.openqa.selenium.OutputType;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebDriverException;
 import org.openqa.selenium.html5.LocalStorage;
@@ -27,10 +29,23 @@ import org.openqa.selenium.html5.Location;
 import org.openqa.selenium.html5.LocationContext;
 import org.openqa.selenium.html5.SessionStorage;
 import org.openqa.selenium.html5.WebStorage;
+import org.openqa.selenium.interactions.TouchScreen;
+import org.openqa.selenium.mobile.NetworkConnection;
+import org.openqa.selenium.remote.Command;
 import org.openqa.selenium.remote.FileDetector;
+import org.openqa.selenium.remote.RemoteTouchScreen;
 import org.openqa.selenium.remote.RemoteWebDriver;
+import org.openqa.selenium.remote.Response;
 import org.openqa.selenium.remote.html5.RemoteLocationContext;
 import org.openqa.selenium.remote.html5.RemoteWebStorage;
+import org.openqa.selenium.remote.mobile.RemoteNetworkConnection;
+
+import java.io.IOException;
+import java.util.Base64;
+import java.util.HashMap;
+import java.util.Map;
+import java.util.logging.Level;
+import java.util.logging.Logger;
 
 /**
  * A {@link WebDriver} implementation that controls a Chrome browser running on the local machine.
@@ -103,8 +118,13 @@ import org.openqa.selenium.remote.html5.RemoteWebStorage;
 public class ChromeDriver extends RemoteWebDriver
     implements LocationContext, WebStorage {
 
+
+  private static final Logger logger = Logger.getLogger(ChromeDriver.class.getName());
+
   private RemoteLocationContext locationContext;
   private RemoteWebStorage webStorage;
+  private TouchScreen touchScreen;
+  private RemoteNetworkConnection networkConnection;
 
   /**
    * Creates a new ChromeDriver using the {@link ChromeDriverService#createDefaultService default}
@@ -170,6 +190,8 @@ public class ChromeDriver extends RemoteWebDriver
     super(new ChromeDriverCommandExecutor(service), capabilities);
     locationContext = new RemoteLocationContext(getExecuteMethod());
     webStorage = new  RemoteWebStorage(getExecuteMethod());
+    this.touchScreen = new RemoteTouchScreen(this.getExecuteMethod());
+    this.networkConnection = new RemoteNetworkConnection(this.getExecuteMethod());
   }
 
   @Override
@@ -199,6 +221,18 @@ public class ChromeDriver extends RemoteWebDriver
     locationContext.setLocation(location);
   }
 
+  public TouchScreen getTouch() {
+      return this.touchScreen;
+  }
+
+  public NetworkConnection.ConnectionType getNetworkConnection() {
+      return this.networkConnection.getNetworkConnection();
+  }
+
+  public NetworkConnection.ConnectionType setNetworkConnection(NetworkConnection.ConnectionType type) {
+      return this.networkConnection.setNetworkConnection(type);
+  }
+
   /**
    * Launches Chrome app specified by id.
    *
@@ -206,5 +240,115 @@ public class ChromeDriver extends RemoteWebDriver
    */
   public void launchApp(String id) {
     execute(ChromeDriverCommand.LAUNCH_APP, ImmutableMap.of("id", id));
+  }
+
+
+  public <X> X getScreenshotAs(OutputType<X> outputType) throws WebDriverException {
+    try {
+      Object
+          visibleSize =
+          evaluate("({x:0,y:0,width:window.innerWidth,height:window.innerHeight})");
+      Long visibleW = jsonValue(visibleSize, "result.value.width", Long.class);
+      Long visibleH = jsonValue(visibleSize, "result.value.height", Long.class);
+
+      Object contentSize = send("Page.getLayoutMetrics", new HashMap<String, Object>());
+      Long cw = jsonValue(contentSize, "contentSize.width", Long.class);
+      Long ch = jsonValue(contentSize, "contentSize.height", Long.class);
+
+              /*
+               * In chrome 61, delivered one day after I wrote this comment, the method forceViewport was removed.
+               * I commented it out here with the if(false), and hopefully wrote a working alternative in the else 8-/
+               */
+      Map<String, Object> parms;
+
+      if (false) {
+        parms = new HashMap<>();
+        parms.put("width", cw);
+        parms.put("height", ch);
+        send("Emulation.setVisibleSize", parms);
+
+        parms = new HashMap<>();
+        parms.put("x", Long.valueOf(0));
+        parms.put("y", Long.valueOf(0));
+        parms.put("scale", Long.valueOf(1));
+
+        send("Emulation.forceViewport", parms);
+      } else {
+        parms = new HashMap<>();
+        parms.put("width", cw);
+        parms.put("height", ch);
+        parms.put("deviceScaleFactor", Long.valueOf(1));
+        parms.put("mobile", Boolean.FALSE);
+        parms.put("fitWindow", Boolean.FALSE);
+        send("Emulation.setDeviceMetricsOverride", parms);
+
+        parms = new HashMap<>();
+        parms.put("width", cw);
+        parms.put("height", ch);
+        send("Emulation.setVisibleSize", parms);
+      }
+
+
+      parms = new HashMap<>();
+      parms.put("format", "png");
+      parms.put("fromSurface", Boolean.TRUE);
+      Object value = send("Page.captureScreenshot", parms);
+
+      // Since chrome 61 this call has disappeared too; it does not seem to be necessary anymore with the new code.
+      // send("Emulation.resetViewport", ImmutableMap.of());
+
+      parms = new HashMap<>();
+      parms.put("x", 0L);
+      parms.put("y", 0L);
+      parms.put("width", visibleW);
+      parms.put("height", visibleH);
+      send("Emulation.setVisibleSize", parms);
+
+      String image = jsonValue(value, "data", String.class);
+      byte[] bytes = Base64.getDecoder().decode(image);
+      return outputType.convertFromPngBytes(bytes);
+    } catch (IOException e) {
+      logger.log(Level.SEVERE, "Could not take screenshot", e);
+    }
+    return null;
+  }
+
+
+
+  private Object evaluate(String script) throws IOException {
+      Map<String, Object> param = new HashMap<>();
+      param.put("returnByValue", Boolean.TRUE);
+      param.put("expression", script);
+
+      return send("Runtime.evaluate", param);
+  }
+
+  private Object send(String cmd, Map<String, Object> params) throws IOException {
+      Map<String, Object> exe = ImmutableMap.of("cmd", cmd, "params", params);
+      Command xc = new Command(getSessionId(), "sendCommandWithResult", exe);
+      Response response = getCommandExecutor().execute(xc);
+
+      Object value = response.getValue();
+      if(response.getStatus() == null || response.getStatus().intValue() != 0) {
+          //System.out.println("resp: " + response);
+          throw new WebDriverException("Command '" + cmd + "' failed: " + value);
+      }
+      if(null == value)
+          throw new WebDriverException("Null response value to command '" + cmd + "'");
+      //System.out.println("resp: " + value);
+      return value;
+  }
+
+  static private <T> T jsonValue(Object map, String path, Class<T> type) {
+      String[] segs = path.split("\\.");
+      Object current = map;
+      for(String name: segs) {
+          Map<String, Object> cm = (Map<String, Object>) current;
+          Object o = cm.get(name);
+          if(null == o)
+              return null;
+          current = o;
+      }
+      return (T) current;
   }
 }
--- java/client/src/org/openqa/selenium/chrome/ChromeDriverCommandExecutor.java
+++ java/client/src/org/openqa/selenium/chrome/ChromeDriverCommandExecutor.java
@@ -34,7 +34,9 @@ class ChromeDriverCommandExecutor extends DriverCommandExecutor {
 
   private static final Map<String, CommandInfo> CHROME_COMMAND_NAME_TO_URL = ImmutableMap.of(
       ChromeDriverCommand.LAUNCH_APP,
-      new CommandInfo("/session/:sessionId/chromium/launch_app", HttpMethod.POST));
+      new CommandInfo("/session/:sessionId/chromium/launch_app", HttpMethod.POST),
+      "sendCommandWithResult", new CommandInfo("/session/:sessionId/chromium/send_command_and_get_result", HttpMethod.POST)
+  );
 
   public ChromeDriverCommandExecutor(DriverService service) {
     super(service, CHROME_COMMAND_NAME_TO_URL);
